bcd0f9b2005b875ed128db0fe17fd0284a76f6cd
jsah-mc <sah.joseph@outlook.com>
2025-11-01 19:18:50 -0400

    Stole sum stuff

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -11,19 +11,20 @@ in {
         programs.firefox = {
             enable = true;
 
-            # Install extensions from NUR
-            extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-                decentraleyes
-                ublock-origin
-                clearurls
-                sponsorblock
-                darkreader
-                h264ify
-                df-youtube
-            ];
+
 
             # Privacy about:config settings
             profiles.jsah-mc = {
+              # Install extensions from NUR
+              extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+                  decentraleyes
+                  ublock-origin
+                  clearurls
+                  sponsorblock
+                  darkreader
+                  h264ify
+                  df-youtube
+                ];
                 settings = {
                     "browser.send_pings" = false;
                     "browser.urlbar.speculativeConnect.enabled" = false;
@@ -88,63 +89,9 @@ in {
                 };
 
                 # userChome.css to make it look better
-                userChrome = "
-                    * { 
-                        box-shadow: none !important;
-                        border: 0px solid !important;
-                    }
-
-                    #tabbrowser-tabs {
-                        --user-tab-rounding: 8px;
-                    }
-
-                    .tab-background {
-                        border-radius: var(--user-tab-rounding) var(--user-tab-rounding) 0px 0px !important; /* Connected */
-                        margin-block: 1px 0 !important; /* Connected */
-                    }
-                    #scrollbutton-up, #scrollbutton-down { /* 6/10/2021 */
-                        border-top-width: 1px !important;
-                        border-bottom-width: 0 !important;
-                    }
-
-                    .tab-background:is([selected], [multiselected]):-moz-lwtheme {
-                        --lwt-tabs-border-color: rgba(0, 0, 0, 0.5) !important;
-                        border-bottom-color: transparent !important;
-                    }
-                    [brighttext='true'] .tab-background:is([selected], [multiselected]):-moz-lwtheme {
-                        --lwt-tabs-border-color: rgba(255, 255, 255, 0.5) !important;
-                        border-bottom-color: transparent !important;
-                    }
-
-                    /* Container color bar visibility */
-                    .tabbrowser-tab[usercontextid] > .tab-stack > .tab-background > .tab-context-line {
-                        margin: 0px max(calc(var(--user-tab-rounding) - 3px), 0px) !important;
-                    }
-
-                    #TabsToolbar, #tabbrowser-tabs {
-                        --tab-min-height: 29px !important;
-                    }
-                    #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar, 
-                    #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar #tabbrowser-tabs {
-                        --tab-min-height: 30px !important;
-                    }
-                    #scrollbutton-up,
-                    #scrollbutton-down {
-                        border-top-width: 0 !important;
-                        border-bottom-width: 0 !important;
-                    }
-
-                    #TabsToolbar, #TabsToolbar > hbox, #TabsToolbar-customization-target, #tabbrowser-arrowscrollbox  {
-                        max-height: calc(var(--tab-min-height) + 1px) !important;
-                    }
-                    #TabsToolbar-customization-target toolbarbutton > .toolbarbutton-icon, 
-                    #TabsToolbar-customization-target .toolbarbutton-text, 
-                    #TabsToolbar-customization-target .toolbarbutton-badge-stack,
-                    #scrollbutton-up,#scrollbutton-down {
-                        padding-top: 7px !important;
-                        padding-bottom: 6px !important;
-                    }
-                ";
+                userChrome = ''
+                   ${builtins.readFile ./chrome.css} 
+                '';
             };
         };
     };
